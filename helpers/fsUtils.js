acc435c0939c8d872685ce04e8f0e41b26295108
Chris Waje <chriswaje324@gmail.com>
2023-11-13 11:58:30 -0800

    ADDED: comments for code functionality

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -3,11 +3,13 @@ const util = require('util');
 
 const readFromFile = util.promisify(fs.readFile);
 
+// code that writes JSON syntax for notes into db.json file
 const writeToFile = (destination, content) =>
 fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
 err ? console.error(err) : console.info(`\nData written to ${destination}`)
 );
 
+// code that reads data and pushes data into db.json file
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const routes = require('./routes/index');
 const PORT = 3001;
 const app = express();
 
+// middleware for app to expect JSON syntax and data passing through URL
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// pathing for static files in public folder
 app.use(express.static('public'));
 
+// path for routes used in reading and writing notes
 app.use('/api', routes);
 
 // GET route for homepage
