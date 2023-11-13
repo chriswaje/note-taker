ea97756acb692f2a66f5834c3e1d6e187d5d1762
Chris Waje <chriswaje324@gmail.com>
2023-11-13 11:27:14 -0800

    ADDED: routes for note page

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router;
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+
+router.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+router.post('/notes', (req, res) => {
+    const { title, text} = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('New note added successfully')
+    } else {
+        res.error('Error in adding note')
+    }
+});
+
+module.exports = router;
